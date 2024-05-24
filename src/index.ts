ce32d60132fd44c0084eaaab5755e1e9e0c799fa
Abiria <abiria.dev@gmail.com>
2024-05-24 09:24:15 +0900

    feat: basic tree building

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log('a')
+#!/usr/bin/env node
+
+import { argv } from 'node:process'
+import { readFile } from 'node:fs/promises'
+import { VisualizerData } from './types.js'
+import { Graph } from './graph.js'
+
+const file = argv[2]
+
+const data = JSON.parse(
+	(await readFile(file)).toString(),
+) as VisualizerData
+
+const graph = new Graph()
+
+Object.entries(data.nodeMetas).forEach(
+	([
+		id,
+		{ id: fileName, isEntry, isExternal, imported },
+	]) => {
+		graph.addNode(id, {
+			fileName,
+			type: isEntry
+				? 'entry'
+				: isExternal
+					? 'external'
+					: 'chunk',
+			imports: imported.map(({ uid }) => uid),
+		})
+	},
+)
+
+console.log(graph.toJson())
