4a55ec2278b064e16d0845e549f3cf465d8b7345
Abiria <abiria.dev@gmail.com>
2024-05-24 10:24:39 +0900

    feat: support colors

---
+++ src/mermaid.ts
@@ -0,0 +1,40 @@
+import { match, P } from 'ts-pattern'
+
+export interface MermaidNodeStyle {
+	color: string
+	shape: MermaidNodeShape
+}
+
+export type MermaidNodeShape =
+	| 'default'
+	| 'round'
+	| 'stadium'
+	| 'circle'
+	| 'rhombus'
+	| 'hexagon'
+
+export const styleMermaidNode = (
+	id: string,
+	text: string = id,
+	{ color, shape }: Partial<MermaidNodeStyle> = {},
+) => {
+	const quotedText = `"${text}"`
+
+	const lines = [
+		`${id}${match(shape)
+			.with(
+				P.union('default', undefined),
+				() => `[${quotedText}]`,
+			)
+			.with('round', () => `(${quotedText})`)
+			.with('stadium', () => `([${quotedText}])`)
+			.with('circle', () => `((${quotedText}))`)
+			.with('rhombus', () => `{${quotedText}}`)
+			.with('hexagon', () => `{{${quotedText}}}`)
+			.exhaustive()}`,
+	]
+
+	if (color) lines.push(`style ${id} stroke:${color}`)
+
+	return lines
+}
--- src/utils.ts
+++ src/utils.ts
@@ -1,32 +1,5 @@
-import { match } from 'ts-pattern'
-
 export const indentHelper = (
 	lines: Array<string>,
 	indent: number = 1,
 	tab: string = '\t',
 ) => lines.map(line => tab.repeat(indent) + line)
-
-export type MermaidNodeStyle =
-	| 'default'
-	| 'round'
-	| 'stadium'
-	| 'circle'
-	| 'rhombus'
-	| 'hexagon'
-
-export const styleMermaidNode = (
-	id: string,
-	text: string = id,
-	style: MermaidNodeStyle = 'default',
-) => {
-	const quotedText = `"${text}"`
-
-	return `${id}${match(style)
-		.with('default', () => `[${quotedText}]`)
-		.with('round', () => `(${quotedText})`)
-		.with('stadium', () => `([${quotedText}])`)
-		.with('circle', () => `((${quotedText}))`)
-		.with('rhombus', () => `{${quotedText}}`)
-		.with('hexagon', () => `{{${quotedText}}}`)
-		.exhaustive()}`
-}
